a4b0e6e9fa01c11403379d216ec69fd04cce5257
Fernando Esquirio Torres <esquirio@gmail.com>
2020-11-06 20:13:54 -0300

    Aula 09

---
+++ Aula_09_Funcoes/Projeto_92_Funcao_02/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//Prototipo da funcao 
+int Quadrado(int num);
+/*Função main */
+int main()
+{
+  int resultado, num01 = 3;
+  resultado = Quadrado(num01); //Chamada da funcao
+  printf("Endereco de num na main: %d \n", &num01);
+  system("PAUSE");
+  printf("O valor do quadrado de 3 eh: %d", resultado);
+  return 0;
+}//Fim da funcao int main()
+
+int Quadrado(int num)
+{
+  int resp;
+  printf("Endereco de num na funcao: %d \n", &num);
+  system("PAUSE");
+  resp = num * num;
+  return resp;
+}//Fim da funcao int quadrado()
---
+++ Aula_09_Funcoes/Projeto_94_Funcao_04/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Prototiopo da funcao
+float Converte_Celsius(float fahr);
+
+//Funcao main()
+int main()
+{
+    float celsius = 0, fahrenheit = 0;
+    printf("Digite a temperatura em graus Fahrenheit: ");
+    scanf("%f", &fahrenheit);
+    printf("%f\n", fahrenheit);
+    //Chamada da funcao
+    celsius = Converte_Celsius(fahrenheit);
+
+    printf("Temperatura em Celsius = %.2f\n", celsius);
+    system("PAUSE");
+    return 0;
+}//Fim da funcao main()
+
+//Funcao float Converte_Celsius(float fahr)
+float Converte_Celsius(float fahr){
+    float c;
+    c = (fahr - 32.0)*5.0/9.0;
+    return c;
+}//Fim float Converte_Celsius(float fahr)
+
