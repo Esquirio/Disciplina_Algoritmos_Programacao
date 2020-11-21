c2789cfc7dd905b1c1b1d89db9e502e2f7ba7f0f
Fernando Esquirio Torres <esquirio@gmail.com>
2020-11-21 08:22:28 -0300

    Aula 11

---
+++ Aula_11_Vetores/Projeto_105_Vetor_01/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int i = 0; //Contador - indice do vetor
+  float notas[10]; //Declara vetor com 10 elementos
+
+  //Laço para leitura das 10 notas e armazenamento do vetor
+  for(i = 0; i < 10; i++){
+      printf("Digite a %i a nota: \n", (i+1));
+      scanf("%f", &notas[i]);
+  }//Fim do for
+
+  //Laço para imprimir os dados do vetor
+  for(i = 0; i < 10; i++){
+    printf("%ia nota: %.1f \n", (i+1), notas[i]);
+  }//Fim do for
+  return 0;
+}//Fim da funcao main()
---
+++ Aula_11_Vetores/Projeto_106_Vetor_02/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int i, maior;
+  //Declaracao do vetor de 5 elementos
+  int numeros[5];
+
+  //Laço para armazenar os dados no vetor
+  for(i = 0; i < 5; i++){
+    printf("Digite um numero: \n");
+    scanf("%i", &numeros[i]);
+  }//Fim do for
+
+  //Atribui o 1o numero do vetor a variável maior
+  maior = numeros[0];
+
+  //Encontra o maior numero
+  for(i = 0; i < 5; i++){
+    if(numeros[i] > maior){
+      maior = numeros[i];
+    }//Fim do if
+  }//Fim do for
+
+  printf("O maior numero eh: %i \n", maior);
+  return 0;
+}//Fim da main()
